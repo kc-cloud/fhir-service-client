6d350905095ffd41fae45b02edee7566b4f2c308
Kannan C <41703213+kc-cloud@users.noreply.github.com>
2024-06-21 11:35:26 -0400

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -1,8 +1,8 @@
 import requests
 
-url = ""
+url = "https://login.microsoftonline.com/{{tenantId}}/oauth2/token"
 
-payload = ''
+payload = 'grant_type=client_credentials&client_id={{client-id}}&client_secret=<client-secret>&resource=https%3A%2F%2Fws4healthdataservice-test-patients-fhir-service.fhir.azurehealthcareapis.com'
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
